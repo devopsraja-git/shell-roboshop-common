0c6cb4908c89f6f6b505b6611a2528ca7b6d6377
Raja Shekar <devops86sraja@gmail.com>
2025-09-26 15:01:47 +0530

    updates files

--- shipping.sh
+++ shipping.sh
@@ -15,9 +15,11 @@ validate $? "Installing mySQL.."
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
     if [ $? -ne 0 ]; then
+        echo -e "Starting to LOAD Shipping services.. "
         mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
         mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOG_FILE
         mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+        echo -e " Shipping Services $G Loaded.. $N"
     else
         echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
     fi
