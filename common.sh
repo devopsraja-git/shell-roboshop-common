e83e05f65e7c56924254c041b16873de5020c043
Raja Shekar <devops86sraja@gmail.com>
2025-09-26 14:29:10 +0530

    updates files

--- common.sh
+++ common.sh
@@ -11,7 +11,7 @@ uid=$(id -u)
 LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-START_TIME=$(date +$S)
+START_TIME=$(date +$s)
 SCRIPT_DIR=$PWD # for absoulute path
 MONGODB_HOST=mongodb.devraxtech.fun
 MYSQL_HOST=mysql.devraxtech.fun
@@ -105,7 +105,7 @@ app_restart(){
 }
 
 print_total_time(){
-    END_TIME=$(date +%S)
+    END_TIME=$(date +%s)
     TOTAL_TIME=$(( $END_TIME - $START_TIME ))
     echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
 }
